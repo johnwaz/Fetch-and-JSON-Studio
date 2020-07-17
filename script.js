99af6ea3e70cbe2e3a2a4ca7c4e45b1609c6b273
John Wasielewicz <wazjohn@yahoo.com>
2020-07-17 09:16:12 -0500

    Was finally able to change the text color to green for astronauts that are still active. Not happy with the way it is coded, but it works.

--- script.js
+++ script.js
@@ -3,8 +3,24 @@ window.addEventListener("load", function() {
   fetch("https://handlers.education.launchcode.org/static/astronauts.json").then(function(response) {
     response.json().then(function(json) {
       let container = '';
+      let status = document.getElementById("status");
       for (let i = 0; i < json.length; i++) {
+        if (json[i].active === true) {
         container = container + `
+          <div class="astronaut">
+            <div class="bio">
+              <h3>${json[i].firstName} ${json[i].lastName}</h3>
+              <ul>
+                <li>Hours in space: ${json[i].hoursInSpace}</li>
+                <li style="color:green;">Active: ${json[i].active}</li>
+                <li>Skills: ${json[i].skills}</li>
+              </ul>
+            </div>
+            <img class="avatar" src=${json[i].picture}>
+          </div>
+          `;
+        } else {
+          container = container + `
           <div class="astronaut">
             <div class="bio">
               <h3>${json[i].firstName} ${json[i].lastName}</h3>
@@ -17,6 +33,7 @@ window.addEventListener("load", function() {
             <img class="avatar" src=${json[i].picture}>
           </div>
           `;
+        }
       }
       document.getElementById("container").innerHTML = container;
       document.getElementById("container").innerHTML += `<h4>Total astronauts: ${json.length}</h4>`;
